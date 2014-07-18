23cad3d2fc6a63332480ad637733a3ce07a0ab34
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-18 17:41:56 +0900

    Add lesson6, Basic Reduce

---
+++ basic_reduce.js
@@ -0,0 +1,8 @@
+module.exports = countWords
+
+function countWords (inputWords) {
+  return inputWords.reduce(function (countMap, word) {
+    countMap[word] = ++countMap[word] || 1
+    return countMap
+  }, {})
+}
