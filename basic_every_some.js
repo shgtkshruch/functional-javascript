254b7b53c122678fcfa3093212ae980218dc1ec0
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-18 17:06:11 +0900

    Add lesson5, Basic Every Some

---
+++ basic_every_some.js
@@ -0,0 +1,11 @@
+module.exports = checkUsersValid
+
+function checkUsersValid (goodUsers) {
+  return function (submittedUsers) {
+    return submittedUsers.every(function (submittedUser) {
+      return goodUsers.some(function (goodUser) {
+        return submittedUser.id === goodUser.id
+      })
+    })
+  }
+}
